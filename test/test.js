4d97f96242ab3d6f4112c6dac2c8e8acf8acd208
Andreas Frank <andreas.frank@gmail.com>
2020-01-10 11:43:00 -0800

    Added mocha test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
-var request = require('supertest');
-var app = require('../app.js');
- 
-describe('GET /', function() {
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+var request = require("supertest");
+var app = require("../app.js");
+
+describe("GET /", function() {
+  it("respond with hello world", done => {
+    request(app)
+      .get("/")
+      .expect("Hello World\n", done);
   });
 });
